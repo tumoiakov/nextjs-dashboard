e850d74437c7da0bf24128c3bce1d5af830a4275
tumoiakov <tumoyakov.work@outlook.com>
2025-09-01 23:09:47 +0700

    chapter 16

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,11 @@
 import { Suspense } from "react";
 import AcmeLogo from "../ui/acme-logo";
 import LoginForm from "../ui/login-form";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
 
 export default function LoginPage() {
 	return (
